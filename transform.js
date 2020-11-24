3f26e1574e60f1aee894bc42b457bc8016b6e6ce
Reto Hugi <reto.hugi@soulcode.ch>
2020-11-25 00:20:10 +0100

    Introduces sorted rings in csv for consistent radar

--- transform.js
+++ transform.js
@@ -7,10 +7,14 @@ const csvInit = "name,ring,quadrant,isNew,description\n"
 const outFile = "radar.csv"
 const workDir = "./content"
 
-function readFiles(directoy, files) {
+function readFiles(directoy, files)
+{
   content = [];
-  for(var i = 0; i < files.length; i++){
-    if (files[i] != "_blip.md") {
+
+  for(var i = 0; i < files.length; i++)
+  {
+    if (files[i] != "_blip.md")
+    {
       console.log("Reading file: " + files[i]);
       content.push(fs.readFileSync(directoy + "/" + files[i], 'utf-8'));
     }
@@ -18,25 +22,63 @@ function readFiles(directoy, files) {
   return content;
 }
 
+function processBlip(blip)
+{
+  // parse blip (includes front matter and body)
+  parsedBlip = parser.parseSync(blip);
+  
+  console.log("Adding entry: " + parsedBlip.data.name);
+
+  // convert markdown to html and remove newlines
+  parsedBlip.content = converter.makeHtml(parsedBlip.content).replace(/(?:\r\n|\r|\n)/g, '');
+  parsedBlip.content = '"' + parsedBlip.content.replace(/["]/g, "\"\"") + '"';
+
+  return parsedBlip;
+}
+
+function getCsvString(a)
+{
+  var tmpString = '';
+
+  for (var i = 0; i<a.length; i++)
+  {
+    tmpString += a[i].data.name + del + a[i].data.ring + del + a[i].data.quadrant + del + a[i].data.isNew.toString().toUpperCase() + del + a[i].content + "\n";
+  }
+  return tmpString;
+}
+
 // get the content of all blips
 blips = readFiles(workDir, fs.readdirSync(workDir));
 
 let csvString = csvInit;
+let adopt = [];
+let trial = [];
+let assess = [];
+let hold = [];
 
-for (var i = 0; i < blips.length; i++) {
-  
-  // parse front matter
-  blip = parser.parseSync(blips[i]);
-  
-  console.log("Adding entry: " + blip.data.name);
+for (var i = 0; i < blips.length; i++)
+{
+  blip = processBlip(blips[i]);
 
-  // convert markdown to html and remove newlines
-  htmlString = converter.makeHtml(blip.content).replace(/(?:\r\n|\r|\n)/g, '');
-  blipContent = '"' + htmlString.replace(/["]/g, "\"\"") + '"';
-
-  // add blip to csv string
-  csvString += blip.data.name + del + blip.data.ring + del + blip.data.quadrant + del + blip.data.isNew.toString().toUpperCase() + del + blipContent + "\n";
+  switch (blip.data.ring) {
+    case "Adopt":
+      adopt.push(blip);
+    case "Trial":
+      trial.push(blip);
+    case "Assess":
+      assess.push(blip);
+    case "Hold":
+      hold.push(blip);
+    default:
+      break;
+  }
 }
+
+// need to make sure we have at least sorted the rings, otherwise the radar renders rubbish.
+csvString += getCsvString(adopt);
+csvString += getCsvString(trial);
+csvString += getCsvString(assess);
+csvString += getCsvString(hold);
 
 console.log("write csv...");
 fs.writeFileSync(outFile, csvString);
